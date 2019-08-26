069919cb33519712948b7a8b74e374e78a54ab45
RamonFontana <fontanaramon@gmail.com>
2019-08-26 01:54:50 -0300

    configurado pagina de login

--- interessado.php
+++ interessado.php
@@ -1,16 +1,20 @@
 <?php
 
-$con = mysqli('localhost','root','root','RCP') or die('Não conseguiu conectar');
+$con = mysqli_connect('localhost','root','root','RCP') or die ('Não conseguiu conectar');
 
 $tipo_interessado = implode(',',$_POST['interesse']);
 
-$query = 'INSERT INTO INTERESSADO(NOME_INTERESSADO, TELEFONE_INTERESSADO, EMAIL_INTERESSADO, DESCRICAO, TIPO_INTERESSADO) VALUE('$_POST['nome_interessado']','$_POST['telefone_interessado']','$_POST['email_interessado']','$_POST['mensagem_Interessado']','$tipo_interessado')';
+$query = ('INSERT INTO INTERESSADO(NOME_INTERESSADO, TELEFONE_INTERESSADO, EMAIL_INTERESSADO, DESCRICAO, TIPO_INTERESSADO) 
+VALUE("'.$_POST['nome_interessado'].'","'.$_POST['telefone_interessado'].'","'.$_POST['email_interessado'].'","'.$_POST['mensagem_Interessado'].'","'.$tipo_interessado.'")');
 
 if (mysqli_query($con, $query)) {
     echo "<script> alert('Dados cadastrados')</script>";
 } else {
-    echo "<script> alert('Erro"mysqli_error($conn)"')</script>";
+    echo 'falho';
 }
 
 mysqli_close($con);
+
+echo "<script>location.href='index.html'</script>"
+
 ?>
